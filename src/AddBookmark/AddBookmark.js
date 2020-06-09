ab6f3c2aad71bc38314959a0eeefc448ac4db806
kidus yilma <kidusyilma@gmail.com>
2020-06-08 20:58:12 -0700

    connected server

---
+++ src/AddBookmark/AddBookmark.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import config from '../config';
+import axios from 'axios';
+import './AddBookmark.css';
+
+const Required = () => <span className="AddBookmark__required">*</span>;
+
+class AddBookmark extends Component {
+	static defaultProps = {
+		onAddBookmark: () => {}
+	};
+
+	state = {
+		error: null
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		// get the form fields from the event
+		const { title, url, description, rating } = e.target;
+		const bookmark = {
+			title: title.value,
+			_url: url.value,
+			_description: description.value,
+			rating: rating.value
+		};
+		console.log(bookmark);
+		this.setState({ error: null });
+
+		axios
+			.post(config.API_ENDPOINT, bookmark, {
+				headers: {
+					'content-type': 'application/json',
+					Authorization: `bearer ${config.API_KEY}`
+				}
+			})
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error("Somthing Went Wrong")       
+         
+				}
+				return res.json();
+			})
+			.then((data) => {
+				title.value = '';
+				url.value = '';
+				description.value = '';
+				rating.value = '';
+				this.props.onAddBookmark(data);
+			})
+			.catch((error) => {
+				this.setState({ error });
+			});
+	};
+
+	render() {
+		const { error } = this.state;
+		const { onClickCancel } = this.props;
+		return (
+			<section className="AddBookmark">
+				<h2>Create a bookmark</h2>
+				<form className="AddBookmark__form" onSubmit={this.handleSubmit}>
+					<div className="AddBookmark__error" role="alert">
+						{error && <p>{error.message}</p>}
+					</div>
+					<div>
+						<label htmlFor="title">
+							Title <Required />
+						</label>
+						<input type="text" name="title" id="title" placeholder="Great website!" required />
+					</div>
+					<div>
+						<label htmlFor="url">
+							URL <Required />
+						</label>
+						<input type="url" name="url" id="url" placeholder="https://www.great-website.com/" required />
+					</div>
+					<div>
+						<label htmlFor="description">Description</label>
+						<textarea name="description" id="description" />
+					</div>
+					<div>
+						<label htmlFor="rating">
+							Rating <Required />
+						</label>
+						<input type="number" name="rating" id="rating" defaultValue="1" min="1" max="5" required />
+					</div>
+					<div className="AddBookmark__buttons">
+						<button type="button" onClick={onClickCancel}>
+							Cancel
+						</button>{' '}
+						<button type="submit">Save</button>
+					</div>
+				</form>
+			</section>
+		);
+	}
+}
+
+export default AddBookmark;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: `http://localhost:8000/bookmarks`,
+  API_KEY: '11asdf21a3d2f138asdf678664',
+}
